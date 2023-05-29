216d4e7fc15ba2bf10dfefe2cd6f209cc9337f9f
aprosdev <hitech.proton@gmail.com>
2023-05-30 04:27:43 +0800

    add click handler on card

--- src/projects/ProjectCard.tsx
+++ src/projects/ProjectCard.tsx
@@ -10,19 +10,30 @@ interface ProjectCardProps {
 }
 
 function ProjectCard(props: ProjectCardProps) {
-  const { project } = props;
-  return (
-    <div className="card">
-      <img src={project.imageUrl} alt={project.name} />
-      <section className="section dark">
-        <h5 className="strong">
-          <strong>{project.name}</strong>
-        </h5>
-        <p>{formatDescription(project.description)}</p>
-        <p>Budget : {project.budget.toLocaleString()}</p>
-      </section>
-    </div>
-  );
+    const { project } = props;
+    const handleEditClick = (projectBeingEdited: Project) => {
+        console.log(projectBeingEdited);
+    };
+
+    return (
+        <div className="card">
+        <img src={project.imageUrl} alt={project.name} />
+        <section className="section dark">
+            <h5 className="strong">
+            <strong>{project.name}</strong>
+            </h5>
+            <p>{formatDescription(project.description)}</p>
+            <p>Budget : {project.budget.toLocaleString()}</p>
+            <button
+                className='bordered' onClick={() => {
+                    handleEditClick(project);
+                }}>
+                <span className='icon-edit'></span>
+                Edit
+            </button>
+        </section>
+        </div>
+    );
 }
 
 export default ProjectCard;
