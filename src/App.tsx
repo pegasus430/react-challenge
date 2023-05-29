a8283a2cbd81349dd525d842fc2af6baeeb4cddf
aprosdev <hitech.proton@gmail.com>
2023-05-30 04:14:42 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
+
+function App() {
+  return (
+    <div className="container">
+      <ProjectsPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Project } from './Project'
+import ProjectCard from './ProjectCard';
+
+interface ProjectListProps {
+    projects: Project[];
+}
+
+function ProjectList({ projects }: ProjectListProps) {
+    return (
+        <div className="row">
+            {projects.map((project) => (
+            <div key={project.id} className="cols-sm">
+                <ProjectCard project={project}></ProjectCard>
+            </div>
+            ))}
+        </div>
+    );
+}
+
+export default ProjectList;
+
